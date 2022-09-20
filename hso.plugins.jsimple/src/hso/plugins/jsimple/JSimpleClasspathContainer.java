ecf44b0d195143cee9603cb7f8542b8197c9ec0a
Hannes Braun <hannesbraun@mail.de>
2022-09-21 00:15:28 +0200

    Resolve classpath container

--- hso.plugins.jsimple/src/hso/plugins/jsimple/JSimpleClasspathContainer.java
+++ hso.plugins.jsimple/src/hso/plugins/jsimple/JSimpleClasspathContainer.java
@@ -1,10 +1,14 @@
 package hso.plugins.jsimple;
 
+import java.io.File;
+
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jdt.core.IClasspathContainer;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.JavaCore;
+import org.osgi.framework.Bundle;
 
 public class JSimpleClasspathContainer implements IClasspathContainer {
 
@@ -12,7 +16,18 @@ public class JSimpleClasspathContainer implements IClasspathContainer {
 
 	@Override
 	public IClasspathEntry[] getClasspathEntries() {
-		return new IClasspathEntry[] { JavaCore.newLibraryEntry(null, null, null) }; // TODO first arg is IPath
+		final Bundle bundle = Platform.getBundle("hso.jsimple");
+		String pathStr = bundle.adapt(File.class).getAbsolutePath();
+
+		// Use the bin directory if we're running the plugin "directly" in a second Eclipse instance. (no JAR)
+		// Is this correct way of doing it? Not sure.
+		pathStr = pathStr.replaceAll("/$", "");
+		if (!pathStr.endsWith(".jar")) {
+			pathStr += System.getProperty("file.separator") + "bin";
+		}
+
+		IPath path = new Path(pathStr);
+		return new IClasspathEntry[] { JavaCore.newLibraryEntry(path, null, null) };
 	}
 
 	@Override
